61b6e9a93c45bd669ee3331a902ce2fcee2378af
Jane <margjane@keemail.me>
2023-05-09 20:35:31 +0800

    change ttt2 menu icon

--- lua/terrortown/menus/gamemode/combattext.lua
+++ lua/terrortown/menus/gamemode/combattext.lua
@@ -1,5 +1,5 @@
 CLGAMEMODEMENU.base = "base_gamemodemenu"
-CLGAMEMODEMENU.icon = Material("vgui/ttt/tid/tid_big_weapon_pistol")
+CLGAMEMODEMENU.icon = Material("vgui/ttt/target_icon")
 CLGAMEMODEMENU.title = "#ttt_combattext.title"
 CLGAMEMODEMENU.description = "TF2-style damage numbers and hit sounds"
 CLGAMEMODEMENU.priority = 1
